cff317665082b530930460fadec73c51fd921f00
Uwe Fischer <141244893+UweFischer-E4U@users.noreply.github.com>
2024-04-23 11:26:37 +0200

    Delete uwe1 directory

--- uwe1/webapp/test/integration/opaTests.qunit.js
+++
@@ -1,7 +0,0 @@
-/* global QUnit */
-
-sap.ui.require(["uwe1/test/integration/AllJourneys"
-], function () {
-	QUnit.config.autostart = false;
-	QUnit.start();
-});
--- uwe1/webapp/test/unit/AllTests.js
+++
@@ -1,5 +0,0 @@
-sap.ui.define([
-	"uwe1/test/unit/controller/uwe1.controller"
-], function () {
-	"use strict";
-});
